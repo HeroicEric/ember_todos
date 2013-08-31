9576781bced4d27ba2811b9fc04fb5d3854e195e
Eric Kelly <heroiceric@gmail.com>
2013-08-31 18:20:58 -0400

    Display number of remaining todos

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -11,5 +11,14 @@ Todos.TodosController = Ember.ArrayController.extend({
 
     this.set('newTitle', '');
     todo.save();
-  }
+  },
+
+  remaining: function() {
+    return this.filterProperty('isCompleted', false).get('length');
+  }.property('@each.isCompleted'),
+
+  inflection: function() {
+    var remaining = this.get('remaining');
+    return remaining === 1 ? "todo" : "todos";
+  }.property('remaining')
 });
