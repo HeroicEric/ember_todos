2b9dbffcf2eabcaf7f8442737b116596edd5845d
Eric Kelly <heroiceric@gmail.com>
2013-08-31 18:03:59 -0400

    Add basic features

--- js/models/todo.js
+++ js/models/todo.js
@@ -2,3 +2,21 @@ Todos.Todo = DS.Model.extend({
   title: DS.attr('string'),
   isCompleted: DS.attr('boolean')
 });
+
+Todos.Todo.FIXTURES = [
+  {
+    id: 1,
+      title: 'Finish this todoozle',
+      isCompleted: true
+  },
+  {
+    id: 2,
+    title: 'build all the ember things',
+    isCompleted: false
+  },
+  {
+    id: 3,
+    title: 'dollar dollar bills',
+    isCompleted: false
+  }
+];
